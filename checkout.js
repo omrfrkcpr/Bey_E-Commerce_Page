bb2a838e775f7ddc49bdaef5c973bdd544e84c93
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-02-12 09:29:54 +0100

    update html names + add initial js variables

---
+++ checkout.js
@@ -0,0 +1,20 @@
+//* ===================================================
+//*                 Checkout Page Solution
+//*  map filter, dest,spread ===================================================
+
+//! Variables that are needed in table
+const shipping = 15.0;
+const tax = 0.18;
+
+let chart = [
+  {
+    name: "Vintage Backpack",
+    price: 34.99,
+    piece: 1,
+    img: "./assets/photo1.png",
+  },
+  { name: "Levi Shoes", price: 40.99, piece: 1, img: "./assets/photo2.png" },
+  { name: "Antique Clock", price: 69.99, piece: 1, img: "./assets/photo3.jpg" },
+];
+
+//! Display on screen
